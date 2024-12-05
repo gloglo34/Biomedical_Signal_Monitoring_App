24ae6dde591772039b4e0f61d470740babcd7506
Gloria Zhou <gloriazhou34@gmail.com>
2024-12-05 18:24:18 +0100

    n/a

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/Auth.js";
 
 dotenv.config();
 
