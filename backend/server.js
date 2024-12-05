96078fb744cda91f580c9b0fa0a9aed23b3a4972
Gloria Zhou <gloriazhou34@gmail.com>
2024-12-05 01:41:17 +0100

    nodemailer

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,39 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to the backend</h1>");
 });
 
+app.get("/callback", (req, res) => {
+  res.send("<h1>Callback url</h1>");
+});
+
 //routes
 app.use("/auth", authRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+import nodemailer from "nodemailer";
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "gloriazhou34@gmail.com",
+    pass: process.env.AppPassword,
+  },
+});
+
+var mailOptions = {
+  from: "gloriazhou34@gmail.com",
+  to: "geezee2510@gmail.com",
+  subject: "Request for authentication",
+  text: "Hey I would like to monitor you remotely, please allow me to acces your fitbit data",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent:" + info.response);
+  }
 });
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -11,6 +11,8 @@ export default function Dashboard() {
     <div>
       <h1>Welcome to the MediTrack Pro Dashboard</h1>
       <p>You are logged in as {sth}</p>
+      <h3>Add patient to begin</h3>
+      <button>Add Patient</button>
       <button onClick={handleLogout}>Log out</button>
     </div>
   );
