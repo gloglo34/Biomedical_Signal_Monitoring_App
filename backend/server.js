9ecf9c89607e364dee48dc5ac02e4cbfaaf3a49a
Gloria Zhou <gloriazhou34@gmail.com>
2024-12-05 19:00:39 +0100

    n/a

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import AuthRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the backend</h1>");
+});
+
+app.get("/callback", (req, res) => {
+  res.send("<h1>Callback url</h1>");
+});
+
+//routes
+app.use("/auth", AuthRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
